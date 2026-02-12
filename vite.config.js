e9aaf0651604d6f8119b5b72828e12c09757af84
Anunay kumar <anunayit@gmail.com>
2026-02-12 22:03:25 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true // 👈 This unlocks the website for your phone!
+  }
+})
